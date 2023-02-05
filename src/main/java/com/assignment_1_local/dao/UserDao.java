22e2aa15e46ed8c13e77f8a276c0b72f97862987
Leon_Baken <Leon_Baken@163.com>
2023-02-04 20:17:24 -0500

    merge feature to main

---
+++ src/main/java/com/assignment_1_local/dao/UserDao.java
@@ -0,0 +1,100 @@
+package com.assignment_1_local.dao;
+
+import com.assignment_1_local.entity.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.List;
+
+@Component
+public class UserDao {
+    public static void createUser(User user) {
+        user.setAccount_created(Timestamp.from(Instant.now()));
+        user.setAccount_updated(Timestamp.from(Instant.now()));
+        Configuration cfg = new Configuration();
+        SessionFactory sf = cfg.configure("hibernate.cfg.xml").buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+        session.persist(user);
+        tx.commit();
+        session.close();
+    }
+
+    public static User getUserById(int id) {
+        Configuration cfg = new Configuration();
+        SessionFactory sf = cfg.configure("hibernate.cfg.xml").buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+        Query query = session.createQuery("from User where id = :id");
+        query.setParameter("id", id);
+        List<User> list = query.list();
+        tx.commit();
+        session.close();
+        return list.get(0);
+    }
+
+    public static User getUserByUsername(String username) {
+        Configuration cfg = new Configuration();
+        SessionFactory sf = cfg.configure("hibernate.cfg.xml").buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+        Query query = session.createQuery("from User where username = :username");
+        query.setParameter("username", username);
+        List<User> list = query.list();
+        tx.commit();
+        session.close();
+        return list.get(0);
+    }
+
+    public static boolean checkUsernameAvailable(String username) {
+        Configuration cfg = new Configuration();
+        SessionFactory sf = cfg.configure("hibernate.cfg.xml").buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+        Query query = session.createQuery("select count (*) from User where username=:username");
+        query.setParameter("username", username);
+        int result = Long.valueOf((long) query.list().get(0)).intValue();
+        tx.commit();
+        session.close();
+        return result == 0;
+    }
+
+    public static boolean checkIdExists(int userId) {
+        Configuration cfg = new Configuration();
+        SessionFactory sf = cfg.configure("hibernate.cfg.xml").buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+        Query query = session.createQuery("select count (*) from User where userId=:userId");
+        query.setParameter("userId", userId);
+        int result = Long.valueOf((long) query.list().get(0)).intValue();
+        tx.commit();
+        session.close();
+        return result != 0;
+    }
+
+    public static void updateUser(User user) {
+        user.setAccount_updated(Timestamp.from(Instant.now()));
+        Configuration cfg = new Configuration();
+        SessionFactory sf = cfg.configure("hibernate.cfg.xml").buildSessionFactory();
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
+        session.merge(user);
+        tx.commit();
+        session.close();
+    }
+
+    public static void main(String[] args) {
+        User user = new User();
+        user.setFirstName("Jun");
+        user.setLastName("Liang");
+        user.setUsername("liang.jun1@northeastern.edu");
+        user.setPassword("123456");
+        createUser(user);
+    }
+}
---
+++ src/main/java/com/assignment_1_local/entity/User.java
@@ -0,0 +1,83 @@
+package com.assignment_1_local.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "assignment_1")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private int userId;
+    private String username;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+    private String firstName;
+    private String lastName;
+//    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Timestamp account_created;
+//    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Timestamp account_updated;
+
+    public User() {
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Timestamp getAccount_created() {
+        return account_created;
+    }
+
+    public void setAccount_created(Timestamp account_created) {
+        this.account_created = account_created;
+    }
+
+    public Timestamp getAccount_updated() {
+        return account_updated;
+    }
+
+    public void setAccount_updated(Timestamp account_updated) {
+        this.account_updated = account_updated;
+    }
+}
