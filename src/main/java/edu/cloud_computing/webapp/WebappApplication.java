aeec1fee7ac97504691f0434c46f14fd40e5a6b7
Jun Liang <Leon_Baken@163.com>
2023-03-01 18:24:15 -0500

    Update WebappApplication.java

--- src/main/java/edu/cloud_computing/webapp/WebappApplication.java
+++ src/main/java/edu/cloud_computing/webapp/WebappApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-//@SpringBootApplication(scanBasePackages = {"edu.cloud_computing.webapp.controller", "edu.cloud_computing.webapp.dao", "edu.cloud_computing.webapp.entity"}, exclude = {SecurityAutoConfiguration.class})
-//public class WebappApplication {
+@SpringBootApplication(scanBasePackages = {"edu.cloud_computing.webapp.controller", "edu.cloud_computing.webapp.dao", "edu.cloud_computing.webapp.entity"}, exclude = {SecurityAutoConfiguration.class})
+public class WebappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebappApplication.class, args);
